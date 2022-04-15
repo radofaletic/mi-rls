491381577b57a608ecf6944e62243f67df7c989a
Rado Faletič <radofaletic@gmail.com>
2022-04-15 15:01:25 +1000

    re-indented source

--- src/100pc.cpp
+++ src/100pc.cpp
@@ -5,14 +5,14 @@
 
 int main(int argc, char* argv[])
 {
-  std::ifstream input(argv[1]);
-  std::ofstream output(argv[2]);
-
-  size_t counter = 0;
-
-  std::string buf;
-  while ( input >> buf )
-    {
-      if ( !((counter++)%100) ) output << buf << std::endl;
-    }
+	std::ifstream input(argv[1]);
+	std::ofstream output(argv[2]);
+	
+	size_t counter = 0;
+	
+	std::string buf;
+	while ( input >> buf )
+	{
+		if ( !((counter++)%100) ) output << buf << std::endl;
+	}
 }
